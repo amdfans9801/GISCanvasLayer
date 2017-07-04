a014a54a3e3476d1c7b6978c5aa228bd2c0f69bd
wenqing <wenqing8215756@163.com>
2017-07-04 10:46:59 +0800

    20170704_10:46

--- src/BaseLayer/BaseMap.js
+++ src/BaseLayer/BaseMap.js
@@ -1,5 +1,16 @@
 define([
-
-], function(){
-
+	"src/Base/declare",
+	"esri/layers/TiledMapServiceLayer",
+	"esri/SpatialReference",
+	"esri/geometry/Extent",
+], function( declare, TiledMapServiceLayer, SpatialReference, Extent ){
+	return declare ( "BaseLayer.BaseMap", TiledMapServiceLayer, {
+		mapUrl: "",
+		//constructor: function (required, required,required, optional)
+		constructor: function (layerType, minLevel, maxLevel, mapurl) {
+			this.type = layerType;
+			this._layerInfo = this._getLayerInfo(layerType);
+			this.spatialReference = new SpatialReference({"wkid": 102100});
+		},
+	});
 });
